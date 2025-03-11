c70942648295da7ac3b90bbe92e9a75f9045c8b4
Ordinarykid-oo <russeldaniel23@gmail.com>
2025-03-11 20:03:13 +0800

    Add files via upload

---
+++ ACT LAB 5.py
@@ -0,0 +1,12 @@
+#LAB ACT 5
+
+age = int(input("age:"))
+if age <= 12:
+    print("you are a child")
+elif age <= 19:
+    print("you are a teen")
+elif age <= 64:
+    print("you are a adult")
+else:
+    print("you are a senior")
+    
